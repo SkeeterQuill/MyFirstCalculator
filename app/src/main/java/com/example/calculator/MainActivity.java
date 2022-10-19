a299e0afbbacde2ab08c895a3698cca12d63239c
Skeet <skeeterquill@outlook.com>
2022-10-19 07:53:03 +0100

    Merge remote-tracking branch 'origin/update_display' into update_display

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,19 +2,110 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.charset.StandardCharsets;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mainDisplay;
+    String numberString = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainDisplay = findViewById(R.id.mainView);
+    }
 
-        float calculation = 0;
+    void updateMainDisplay(int x){
+        if(numberString.equals("0")){
+            numberString = String.valueOf(x);
+        }else if(numberString.length() >= 11){
 
-        String calculationHist = "";
+        }
+        else{
+            numberString = numberString + String.valueOf(x);
+        }
+        mainDisplay.setText(numberString);
+    }
+
+    public void btn_1(View v) {
+        updateMainDisplay(1);
+    }
+
+    public void btn_2(View v) {
+        updateMainDisplay(2);
+    }
+
+    public void btn_3(View v) {
+        updateMainDisplay(3);
+    }
+
+    public void btn_4(View v) {
+        updateMainDisplay(4);
+    }
+
+    public void btn_5(View v) {
+        updateMainDisplay(5);
+    }
+
+    public void btn_6(View v) {
+        updateMainDisplay(6);
+    }
+
+    public void btn_7(View v) {
+        updateMainDisplay(7);
+    }
+
+    public void btn_8(View v) {
+        updateMainDisplay(8);
+    }
+
+    public void btn_9(View v) {
+        updateMainDisplay(9);
+    }
+
+    public void btn_0(View v) {
+        updateMainDisplay(0);
+    }
+
+
+
+
+    public void btn_Backspace(View v) {
+
+        if(numberString.length() == 1){
+            numberString = "0";
+            mainDisplay.setText(numberString);
+
+
+        }else if(numberString.length() > 1){
+            StringBuilder numberS = new StringBuilder(numberString);
+
+            numberS.deleteCharAt(numberString.length()-1);
+
+            numberString = numberS.toString();
+
+
+
+            mainDisplay.setText((numberString));
+        }else{
+
+            numberString = "0";
+            mainDisplay.setText(numberString);
+        }
+
+
+
 
 
     }
+
 }
